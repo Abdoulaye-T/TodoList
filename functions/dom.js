7abe53b193074830f6138a7cb11795a1e6933fce
Abdoulaye Traoré <at904141@gmail.com>
2024-08-17 14:24:29 +0000

    Initial version

---
+++ functions/dom.js
@@ -0,0 +1,28 @@
+//Les fonctions associees au DOM
+
+/**
+ * 
+ * @param {string} tagName
+ * @param {Object} attributes
+ * @return {HTMLElement}
+ * 
+ */
+export function createElement(tagName, attributes = {}) {
+    const element = document.createElement(tagName)
+    for (const [attribute, value] of Object.entries(attributes)) {
+        if(value !== null) {
+            element.setAttribute(attribute, value)
+        }
+        
+    }
+    return element
+
+}
+
+   /**
+     * @param {string} id
+     * @return {DocumentFragment}
+     */
+// export function cloneTemplate (id) {
+//     return document.getElementById(id).content.cloneNode(true)
+//  }
